71adf288bdf0f14af56ba752c9a2973cfb22097d
P4RKER <148176991+muhamadrayhan23@users.noreply.github.com>
2025-05-21 20:27:42 +0700

    Update tambah.php

--- Views/tugas/tambah.php
+++ Views/tugas/tambah.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="UTF-8" />
-    <title>Tambah Tugas</title> <!-- Judul halaman -->
+    <title>Tambah Tugas</title> 
     <!-- Link Bootstrap 5 -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
