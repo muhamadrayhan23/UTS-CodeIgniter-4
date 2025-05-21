ad8c28b788c4959304df421218b30d3ecd88e102
P4RKER <148176991+muhamadrayhan23@users.noreply.github.com>
2025-05-21 20:26:59 +0700

    Update edit.php

--- Views/tugas/edit.php
+++ Views/tugas/edit.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="UTF-8" />
-    <title>Edit Tugas</title> <!-- Judul tab halaman browser -->
+    <title>Edit Tugas</title> 
     <!-- Link Bootstrap 5 -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
